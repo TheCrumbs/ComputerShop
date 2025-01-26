c6e91a28c6fa14981d162cfaefef58f38dbdf2ea
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 14:46:52 -0500

    Update Keyboard.java
    reasdassgdfbxcvbfwsdcsfadfh

--- shop/parts/Keyboard.java
+++ shop/parts/Keyboard.java
@@ -6,14 +6,15 @@ public class Keyboard extends Part {
     private String brand;
     private String layout;
     private String switchType;
-      private boolean hasRGB; //Added boolean for rgb
+    private boolean hasRGB;
 
-    public Keyboard(String quality, double price, double purchasePrice, String brand, String layout, String switchType, boolean hasRGB) {
+    public Keyboard(String quality, double price, double purchasePrice, 
+                    String brand, String layout, String switchType, boolean hasRGB) {
         super("Keyboard", quality, price, purchasePrice);
         this.brand = brand;
         this.layout = layout;
         this.switchType = switchType;
-          this.hasRGB = hasRGB;
+        this.hasRGB = hasRGB;
     }
 
     public String getBrand() {
@@ -29,35 +30,32 @@ public class Keyboard extends Part {
     }
 
     public boolean hasRGB() {
-         return hasRGB;
-     }
-
-       @Override
-    public double calculateResaleValue() {
-          double baseResaleValue = super.calculateResaleValue();
-         double qualityMultiplier = 1.0;
-            switch (getQuality()) {
-                case QUALITY_FACTORY_NEW:
-                    qualityMultiplier = 1.2;
-                    break;
-                case QUALITY_SLIGHTLY_DAMAGED:
-                     qualityMultiplier = 0.8;
-                    break;
-                case QUALITY_DAMAGED:
-                    qualityMultiplier = 0.6;
-                    break;
-                 case QUALITY_VERY_DAMAGED:
-                   qualityMultiplier = 0.4;
-                    break;
-            }
-
-
-        double rgbMultiplier = hasRGB ? 1.05 : 1.0;
-
-         return  baseResaleValue * qualityMultiplier * rgbMultiplier;
-
+        return hasRGB;
     }
 
+    @Override
+    public double calculateResaleValue() {
+        double baseResaleValue = super.calculateResaleValue();
+        double qualityMultiplier = 1.0;
+        
+        switch (getQuality()) {
+            case QUALITY_FACTORY_NEW:
+                qualityMultiplier = 1.2;
+                break;
+            case QUALITY_SLIGHTLY_DAMAGED:
+                qualityMultiplier = 0.8;
+                break;
+            case QUALITY_DAMAGED:
+                qualityMultiplier = 0.6;
+                break;
+            case QUALITY_VERY_DAMAGED:
+                qualityMultiplier = 0.4;
+                break;
+        }
+
+        double rgbMultiplier = hasRGB ? 1.05 : 1.0;
+        return baseResaleValue * qualityMultiplier * rgbMultiplier;
+    }
 
     @Override
     public String toString() {
@@ -65,7 +63,7 @@ public class Keyboard extends Part {
                 "brand='" + brand + '\'' +
                 ", layout='" + layout + '\'' +
                 ", switchType='" + switchType + '\'' +
-                  ", hasRGB=" + hasRGB +
+                ", hasRGB=" + hasRGB +
                 "} " + super.toString();
     }
 }
