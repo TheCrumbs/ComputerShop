160d856d96d0fd7c3c6d3c2fb68a73d768985f72
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 14:49:04 -0500

    Update Motherboard.java
    Removed all inline comments
    
    Fixed inconsistent indentation (4 spaces per level)
    
    Aligned variable declarations
    
    Standardized method declarations and braces
    
    Formatted constructor parameters to wrap neatly
    
    Fixed switch statement indentation
    
    Corrected toString() formatting and quotation marks
    
    Added missing quotes for chipset and pciSlots in toString()
    
    Standardized spacing around operators and method parameters

--- shop/parts/Motherboard.java
+++ shop/parts/Motherboard.java
@@ -6,17 +6,17 @@ public class Motherboard extends Part {
     private String brand;
     private String socketType;
     private String formFactor;
-    private int ramSlots; // Added number of RAM slots
-      private String chipset; // Added chipset information
-       private String pciSlots;
+    private int ramSlots;
+    private String chipset;
+    private String pciSlots;
 
-
-    public Motherboard(String quality, double price, double purchasePrice, String brand, String socketType, String formFactor, int ramSlots, String chipset, String pciSlots) {
+    public Motherboard(String quality, double price, double purchasePrice, String brand, String socketType,
+                       String formFactor, int ramSlots, String chipset, String pciSlots) {
         super("Motherboard", quality, price, purchasePrice);
         this.brand = brand;
         this.socketType = socketType;
         this.formFactor = formFactor;
-         this.ramSlots = ramSlots;
+        this.ramSlots = ramSlots;
         this.chipset = chipset;
         this.pciSlots = pciSlots;
     }
@@ -32,43 +32,42 @@ public class Motherboard extends Part {
     public String getFormFactor() {
         return formFactor;
     }
-      public int getRamSlots() {
+
+    public int getRamSlots() {
         return ramSlots;
     }
 
-       public String getChipset(){
-           return chipset;
-       }
-         public String getPciSlots(){
-            return pciSlots;
+    public String getChipset() {
+        return chipset;
+    }
+
+    public String getPciSlots() {
+        return pciSlots;
+    }
+
+    @Override
+    public double calculateResaleValue() {
+        double baseResaleValue = super.calculateResaleValue();
+        double qualityMultiplier = 1.0;
+        
+        switch (getQuality()) {
+            case QUALITY_FACTORY_NEW:
+                qualityMultiplier = 1.2;
+                break;
+            case QUALITY_SLIGHTLY_DAMAGED:
+                qualityMultiplier = 0.8;
+                break;
+            case QUALITY_DAMAGED:
+                qualityMultiplier = 0.6;
+                break;
+            case QUALITY_VERY_DAMAGED:
+                qualityMultiplier = 0.4;
+                break;
         }
-
-         @Override
-        public double calculateResaleValue() {
-            double baseResaleValue = super.calculateResaleValue();
-            double qualityMultiplier = 1.0;
-                switch (getQuality()) {
-                    case QUALITY_FACTORY_NEW:
-                        qualityMultiplier = 1.2;
-                        break;
-                    case QUALITY_SLIGHTLY_DAMAGED:
-                        qualityMultiplier = 0.8;
-                        break;
-                     case QUALITY_DAMAGED:
-                        qualityMultiplier = 0.6;
-                        break;
-                     case QUALITY_VERY_DAMAGED:
-                        qualityMultiplier = 0.4;
-                         break;
-                }
-
 
         double ramSlotPerformance = (double) ramSlots / 4;
-
-
-            return baseResaleValue * qualityMultiplier + baseResaleValue * ramSlotPerformance;
-
-        }
+        return baseResaleValue * qualityMultiplier + baseResaleValue * ramSlotPerformance;
+    }
 
     @Override
     public String toString() {
@@ -76,9 +75,9 @@ public class Motherboard extends Part {
                 "brand='" + brand + '\'' +
                 ", socketType='" + socketType + '\'' +
                 ", formFactor='" + formFactor + '\'' +
-                 ", ramSlots=" + ramSlots +
-                    ", chipset=" + chipset +
-                ", pciSlots=" + pciSlots +
+                ", ramSlots=" + ramSlots +
+                ", chipset='" + chipset + '\'' +
+                ", pciSlots='" + pciSlots + '\'' +
                 "} " + super.toString();
     }
 }
