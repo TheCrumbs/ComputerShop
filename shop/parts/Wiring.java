6572b92ab580ec26e2a28fb2583020ef53864c29
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 14:55:09 -0500

    Update Wiring.java
    Removed all unnecessary whitespace and random indentation
    
    Standardized indentation to 4 spaces per level
    
    Aligned switch-case statements with proper indentation
    
    Fixed method spacing and brace placement
    
    Formatted string concatenation in toString() to use standard line breaks
    
    Removed redundant blank lines
    
    Fixed spacing around operators and parameters where needed

--- shop/parts/Wiring.java
+++ shop/parts/Wiring.java
@@ -19,29 +19,27 @@ public class Wiring extends Part {
     public double getLength() {
         return length;
     }
-    
+
     @Override
-    public double calculateResaleValue(){
-    	double baseResaleValue = super.calculateResaleValue();
-    	 double qualityMultiplier = 1.0;
-         switch (getQuality()) {
-             case QUALITY_FACTORY_NEW:
+    public double calculateResaleValue() {
+        double baseResaleValue = super.calculateResaleValue();
+        double qualityMultiplier = 1.0;
+        switch (getQuality()) {
+            case QUALITY_FACTORY_NEW:
                 qualityMultiplier = 1.2;
                 break;
             case QUALITY_SLIGHTLY_DAMAGED:
-                 qualityMultiplier = 0.8;
+                qualityMultiplier = 0.8;
                 break;
-           case QUALITY_DAMAGED:
+            case QUALITY_DAMAGED:
                 qualityMultiplier = 0.6;
-               break;
-           case QUALITY_VERY_DAMAGED:
-                 qualityMultiplier = 0.4;
-               break;
-         }
-         
-         
-          double lengthPerformance =  length / 2;
-    	 return baseResaleValue * qualityMultiplier + baseResaleValue * lengthPerformance;
+                break;
+            case QUALITY_VERY_DAMAGED:
+                qualityMultiplier = 0.4;
+                break;
+        }
+        double lengthPerformance = length / 2;
+        return baseResaleValue * qualityMultiplier + baseResaleValue * lengthPerformance;
     }
 
     @Override
