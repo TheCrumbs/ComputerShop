ed15e7c0535a76175356fa0524113eeffdbd904b
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 14:38:09 -0500

    Update CPU.java
    Removed all inline comments
    
    Fixed inconsistent indentation (now using 4 spaces per level)
    
    Aligned method parameters in constructor
    
    Fixed spacing around method parentheses and braces
    
    Standardized switch-case indentation
    
    Aligned multi-line return statement in calculateResaleValue()
    
    Fixed toString() formatting for consistent parameter alignment
    
    Corrected spacing in method declarations
    
    Removed unnecessary empty lines between tightly related code blocks

--- shop/parts/CPU.java
+++ shop/parts/CPU.java
@@ -6,11 +6,12 @@ public class CPU extends Part {
     private String brand;
     private String model;
     private int generation;
-    private int cores;  // Added number of cores
-    private double clockSpeed; // Added clock speed in GHz
-    private double cacheSize; // Added cache size in MB
+    private int cores;
+    private double clockSpeed;
+    private double cacheSize;
 
-    public CPU(String quality, double price, double purchasePrice, String brand, String model, int generation, int cores, double clockSpeed, double cacheSize) {
+    public CPU(String quality, double price, double purchasePrice, String brand, 
+              String model, int generation, int cores, double clockSpeed, double cacheSize) {
         super("CPU", quality, price, purchasePrice);
         this.brand = brand;
         this.model = model;
@@ -32,47 +33,45 @@ public class CPU extends Part {
         return generation;
     }
 
-     public int getCores() {
+    public int getCores() {
         return cores;
     }
 
     public double getClockSpeed() {
         return clockSpeed;
     }
-    
-      public double getCacheSize(){
-      return cacheSize;
-      }
+
+    public double getCacheSize() {
+        return cacheSize;
+    }
 
     @Override
     public double calculateResaleValue() {
-         double baseResaleValue = super.calculateResaleValue();
-          double qualityMultiplier = 1.0;
-            switch (getQuality()) {
-                case QUALITY_FACTORY_NEW:
-                    qualityMultiplier = 1.2;
-                    break;
-                case QUALITY_SLIGHTLY_DAMAGED:
-                    qualityMultiplier = 0.8;
-                    break;
-                 case QUALITY_DAMAGED:
-                     qualityMultiplier = 0.6;
-                     break;
-                 case QUALITY_VERY_DAMAGED:
-                    qualityMultiplier = 0.4;
-                     break;
+        double baseResaleValue = super.calculateResaleValue();
+        double qualityMultiplier = 1.0;
+        
+        switch (getQuality()) {
+            case QUALITY_FACTORY_NEW:
+                qualityMultiplier = 1.2;
+                break;
+            case QUALITY_SLIGHTLY_DAMAGED:
+                qualityMultiplier = 0.8;
+                break;
+            case QUALITY_DAMAGED:
+                qualityMultiplier = 0.6;
+                break;
+            case QUALITY_VERY_DAMAGED:
+                qualityMultiplier = 0.4;
+                break;
+        }
 
-            }
+        double corePerformance = (double) cores / 10;
+        double clockSpeedPerformance = clockSpeed / 5.0;
+        double cacheSizePerformance = cacheSize / 10.0;
 
-           double corePerformance = (double) cores / 10;
-            double clockSpeedPerformance =  clockSpeed / 5.0;
-            double cacheSizePerformance = cacheSize / 10.0;
-
-
-            return baseResaleValue * qualityMultiplier + baseResaleValue * (corePerformance + clockSpeedPerformance + cacheSizePerformance);
-
+        return baseResaleValue * qualityMultiplier 
+             + baseResaleValue * (corePerformance + clockSpeedPerformance + cacheSizePerformance);
     }
-
 
     @Override
     public String toString() {
@@ -80,9 +79,9 @@ public class CPU extends Part {
                 "brand='" + brand + '\'' +
                 ", model='" + model + '\'' +
                 ", generation=" + generation +
-                  ", cores=" + cores +
+                ", cores=" + cores +
                 ", clockSpeed=" + clockSpeed +
-                  ", cacheSize=" + cacheSize +
+                ", cacheSize=" + cacheSize +
                 "} " + super.toString();
     }
 }
