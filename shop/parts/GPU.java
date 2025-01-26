796e6905dca980526fbf2628383044c2f34272cf
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 14:45:02 -0500

    Update GPU.java
    Removed all comments
    
    Fixed inconsistent indentation (now using 4 spaces throughout)
    
    Aligned method modifiers and annotations
    
    Standardized spacing around operators and parentheses
    
    Fixed toString() concatenation formatting
    
    Normalized switch statement formatting
    
    Split long return statement for better readability
    
    Removed redundant whitespace
    
    Ensured consistent line wrapping practices

--- shop/parts/GPU.java
+++ shop/parts/GPU.java
@@ -6,7 +6,7 @@ public class GPU extends Part {
     private String brand;
     private String model;
     private int memory;
-    private double clockSpeed; //Added clock speed in GHz
+    private double clockSpeed;
 
     public GPU(String quality, double price, double purchasePrice, String brand, String model, int memory, double clockSpeed) {
         super("GPU", quality, price, purchasePrice);
@@ -28,36 +28,36 @@ public class GPU extends Part {
         return memory;
     }
 
-      public double getClockSpeed() {
+    public double getClockSpeed() {
         return clockSpeed;
     }
+
     @Override
-     public double calculateResaleValue(){
-      double baseResaleValue = super.calculateResaleValue();
-      double qualityMultiplier = 1.0;
-      switch(getQuality()){
-          case QUALITY_FACTORY_NEW:
-               qualityMultiplier = 1.2;
-              break;
-          case QUALITY_SLIGHTLY_DAMAGED:
-              qualityMultiplier = 0.8;
-              break;
-          case QUALITY_DAMAGED:
-             qualityMultiplier = 0.6;
-             break;
-          case QUALITY_VERY_DAMAGED:
-              qualityMultiplier = 0.4;
-              break;
-      }
+    public double calculateResaleValue() {
+        double baseResaleValue = super.calculateResaleValue();
+        double qualityMultiplier = 1.0;
+        
+        switch (getQuality()) {
+            case QUALITY_FACTORY_NEW:
+                qualityMultiplier = 1.2;
+                break;
+            case QUALITY_SLIGHTLY_DAMAGED:
+                qualityMultiplier = 0.8;
+                break;
+            case QUALITY_DAMAGED:
+                qualityMultiplier = 0.6;
+                break;
+            case QUALITY_VERY_DAMAGED:
+                qualityMultiplier = 0.4;
+                break;
+        }
 
-          double memoryPerformance = (double) memory / 10;
-           double clockSpeedPerformance = clockSpeed / 3;
+        double memoryPerformance = (double) memory / 10;
+        double clockSpeedPerformance = clockSpeed / 3;
 
-
-      return baseResaleValue * qualityMultiplier + baseResaleValue * (memoryPerformance + clockSpeedPerformance);
-
-     }
-
+        return baseResaleValue * qualityMultiplier 
+                + baseResaleValue * (memoryPerformance + clockSpeedPerformance);
+    }
 
     @Override
     public String toString() {
@@ -65,7 +65,7 @@ public class GPU extends Part {
                 "brand='" + brand + '\'' +
                 ", model='" + model + '\'' +
                 ", memory=" + memory +
-                 ", clockSpeed=" + clockSpeed +
+                ", clockSpeed=" + clockSpeed +
                 "} " + super.toString();
     }
 }
