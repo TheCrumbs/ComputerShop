50cab1a55ecdb7e504a7063b59383e7a5c3e387f
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 15:08:43 -0500

    Update Game.java
    asdasdasdasdasdasdasdasdasasdasdasdasd

--- shop/Game.java
+++ shop/Game.java
@@ -1,52 +1,53 @@
 package shop;
 
 import shop.parts.*;
+
 public class Game {
-      private String name;
-     private CPU requiredCPU;
-      private GPU requiredGPU;
-       private RAM requiredRam;
-      private Motherboard requiredMotherboard;
-       private SSD requiredSsd;
-      private HDD requiredHdd;
-     private boolean hasMonitor;
-     private boolean hasKeyboard;
-      private boolean hasMouse;
-     private boolean hasWiring;
+    private String name;
+    private CPU requiredCPU;
+    private GPU requiredGPU;
+    private RAM requiredRam;
+    private Motherboard requiredMotherboard;
+    private SSD requiredSsd;
+    private HDD requiredHdd;
+    private boolean hasMonitor;
+    private boolean hasKeyboard;
+    private boolean hasMouse;
+    private boolean hasWiring;
 
-
-     public Game(){
+    public Game() {
         this.name = "Generic Game";
-         this.requiredCPU = null;
-          this.requiredGPU = null;
-           this.requiredRam = null;
-           this.requiredMotherboard = null;
-          this.requiredSsd = null;
+        this.requiredCPU = null;
+        this.requiredGPU = null;
+        this.requiredRam = null;
+        this.requiredMotherboard = null;
+        this.requiredSsd = null;
         this.requiredHdd = null;
-       this.hasMonitor = false;
-       this.hasKeyboard = false;
+        this.hasMonitor = false;
+        this.hasKeyboard = false;
         this.hasMouse = false;
-       this.hasWiring = false;
-     }
-
-
-      public Game(String name, CPU requiredCPU, GPU requiredGPU, RAM requiredRam, Motherboard requiredMotherboard, SSD requiredSsd, HDD requiredHdd, boolean hasMonitor, boolean hasKeyboard, boolean hasMouse, boolean hasWiring){
-       this.name = name;
-         this.requiredCPU = requiredCPU;
-         this.requiredGPU = requiredGPU;
-          this.requiredRam = requiredRam;
-            this.requiredMotherboard = requiredMotherboard;
-           this.requiredSsd = requiredSsd;
-          this.requiredHdd = requiredHdd;
-         this.hasMonitor = hasMonitor;
-           this.hasKeyboard = hasKeyboard;
-        this.hasMouse = hasMouse;
-       this.hasWiring = hasWiring;
+        this.hasWiring = false;
     }
-       public String getName() {
+
+    public Game(String name, CPU requiredCPU, GPU requiredGPU, RAM requiredRam, Motherboard requiredMotherboard, SSD requiredSsd, HDD requiredHdd, boolean hasMonitor, boolean hasKeyboard, boolean hasMouse, boolean hasWiring) {
+        this.name = name;
+        this.requiredCPU = requiredCPU;
+        this.requiredGPU = requiredGPU;
+        this.requiredRam = requiredRam;
+        this.requiredMotherboard = requiredMotherboard;
+        this.requiredSsd = requiredSsd;
+        this.requiredHdd = requiredHdd;
+        this.hasMonitor = hasMonitor;
+        this.hasKeyboard = hasKeyboard;
+        this.hasMouse = hasMouse;
+        this.hasWiring = hasWiring;
+    }
+
+    public String getName() {
         return name;
     }
-     public CPU getRequiredCPU() {
+
+    public CPU getRequiredCPU() {
         return requiredCPU;
     }
 
@@ -54,19 +55,23 @@ public class Game {
         return requiredGPU;
     }
 
-     public RAM getRequiredRam() {
+    public RAM getRequiredRam() {
         return requiredRam;
     }
-    public Motherboard getRequiredMotherboard(){
-    return requiredMotherboard;
-  }
-    public SSD getRequiredSsd(){
-     return requiredSsd;
+
+    public Motherboard getRequiredMotherboard() {
+        return requiredMotherboard;
     }
-     public HDD getRequiredHdd(){
-         return requiredHdd;
-      }
-       public boolean isHasMonitor() {
+
+    public SSD getRequiredSsd() {
+        return requiredSsd;
+    }
+
+    public HDD getRequiredHdd() {
+        return requiredHdd;
+    }
+
+    public boolean isHasMonitor() {
         return hasMonitor;
     }
 
@@ -74,11 +79,28 @@ public class Game {
         return hasKeyboard;
     }
 
-     public boolean isHasMouse() {
+    public boolean isHasMouse() {
         return hasMouse;
     }
-     public boolean isHasWiring(){
-       return hasWiring;
-     }
 
+    public boolean isHasWiring() {
+        return hasWiring;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "name='" + name + '\'' +
+                ", requiredCPU=" + requiredCPU +
+                ", requiredGPU=" + requiredGPU +
+                ", requiredRam=" + requiredRam +
+                ", requiredMotherboard=" + requiredMotherboard +
+                ", requiredSsd=" + requiredSsd +
+                ", requiredHdd=" + requiredHdd +
+                ", hasMonitor=" + hasMonitor +
+                ", hasKeyboard=" + hasKeyboard +
+                ", hasMouse=" + hasMouse +
+                ", hasWiring=" + hasWiring +
+                '}';
+    }
 }
