0bcfd21cff406d946780ca71b29e4cd71fcb1d02
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 14:46:02 -0500

    Update HDD.java
    im getting bored of writing actual descriptions ngl

--- shop/parts/HDD.java
+++ shop/parts/HDD.java
@@ -6,14 +6,14 @@ public class HDD extends Part {
     private String brand;
     private int capacity;
     private int rpm;
-      private String type;
+    private String type;
 
     public HDD(String quality, double price, double purchasePrice, String brand, int capacity, int rpm, String type) {
         super("HDD", quality, price, purchasePrice);
         this.brand = brand;
         this.capacity = capacity;
         this.rpm = rpm;
-         this.type = type;
+        this.type = type;
     }
 
     public String getBrand() {
@@ -27,36 +27,35 @@ public class HDD extends Part {
     public int getRpm() {
         return rpm;
     }
-    
+
     public String getType() {
-    	return type;
+        return type;
     }
-    
-      @Override
-     public double calculateResaleValue(){
-         double baseResaleValue = super.calculateResaleValue();
-       double qualityMultiplier = 1.0;
-      switch(getQuality()){
-          case QUALITY_FACTORY_NEW:
-               qualityMultiplier = 1.2;
-              break;
-          case QUALITY_SLIGHTLY_DAMAGED:
-              qualityMultiplier = 0.8;
-              break;
-           case QUALITY_DAMAGED:
-             qualityMultiplier = 0.6;
-             break;
-          case QUALITY_VERY_DAMAGED:
-              qualityMultiplier = 0.4;
-              break;
-      }
 
+    @Override
+    public double calculateResaleValue() {
+        double baseResaleValue = super.calculateResaleValue();
+        double qualityMultiplier = 1.0;
+        switch (getQuality()) {
+            case QUALITY_FACTORY_NEW:
+                qualityMultiplier = 1.2;
+                break;
+            case QUALITY_SLIGHTLY_DAMAGED:
+                qualityMultiplier = 0.8;
+                break;
+            case QUALITY_DAMAGED:
+                qualityMultiplier = 0.6;
+                break;
+            case QUALITY_VERY_DAMAGED:
+                qualityMultiplier = 0.4;
+                break;
+        }
 
-        double capacityPerformance = (double) capacity / 2000; //Convert to TB
-         double rpmPerformance = (double) rpm / 7200;
+        double capacityPerformance = (double) capacity / 2000;
+        double rpmPerformance = (double) rpm / 7200;
 
-    return baseResaleValue * qualityMultiplier + baseResaleValue * (capacityPerformance + rpmPerformance);
-      }
+        return baseResaleValue * qualityMultiplier + baseResaleValue * (capacityPerformance + rpmPerformance);
+    }
 
     @Override
     public String toString() {
@@ -64,7 +63,7 @@ public class HDD extends Part {
                 "brand='" + brand + '\'' +
                 ", capacity=" + capacity +
                 ", rpm=" + rpm +
-                   ", type=" + type +
+                ", type=" + type +
                 "} " + super.toString();
     }
 }
