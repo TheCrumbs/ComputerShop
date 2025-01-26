bfaecbad310ea1e267045901262653a13468758a
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 14:39:23 -0500

    Update Case.java
    Removed all comments (though there weren't any in the original code)
    
    Fixed inconsistent indentation (now using 4 spaces throughout)
    
    Standardized spacing around methods and brackets
    
    Aligned constructor parameters vertically
    
    Fixed spacing in method declarations and invocations
    
    Standardized switch-case indentation
    
    Improved vertical alignment of related code blocks
    
    Added proper spacing around operators
    
    Fixed getDriveBays() method formatting
    
    Standardized line wrapping in toString() method
    
    Added consistent blank lines between methods
    
    Fixed parameter alignment in constructor declaration

--- shop/parts/Case.java
+++ shop/parts/Case.java
@@ -8,8 +8,8 @@ public class Case extends Part {
     private String color;
     private int driveBays;
 
-    public Case(String quality, double price, double purchasePrice, 
-               String brand, String formFactor, String color, int driveBays) {
+    public Case(String quality, double price, double purchasePrice,
+                String brand, String formFactor, String color, int driveBays) {
         super("Case", quality, price, purchasePrice);
         this.brand = brand;
         this.formFactor = formFactor;
@@ -28,32 +28,34 @@ public class Case extends Part {
     public String getColor() {
         return color;
     }
-     public int getDriveBays(){
-      return driveBays;
-     }
-     @Override
-    public double calculateResaleValue(){
-         double baseResaleValue = super.calculateResaleValue();
+
+    public int getDriveBays() {
+        return driveBays;
+    }
+
+    @Override
+    public double calculateResaleValue() {
+        double baseResaleValue = super.calculateResaleValue();
         double qualityMultiplier = 1.0;
-         switch (getQuality()){
-             case QUALITY_FACTORY_NEW:
-                 qualityMultiplier = 1.2;
-                 break;
-              case QUALITY_SLIGHTLY_DAMAGED:
-                 qualityMultiplier = 0.8;
-                 break;
-            case QUALITY_DAMAGED:
-                 qualityMultiplier = 0.6;
+        
+        switch (getQuality()) {
+            case QUALITY_FACTORY_NEW:
+                qualityMultiplier = 1.2;
                 break;
-              case QUALITY_VERY_DAMAGED:
-                 qualityMultiplier = 0.4;
-                  break;
-         }
-            double bayPerformance = (double) driveBays / 4;
-
-
-         return baseResaleValue * qualityMultiplier + baseResaleValue * bayPerformance;
-     }
+            case QUALITY_SLIGHTLY_DAMAGED:
+                qualityMultiplier = 0.8;
+                break;
+            case QUALITY_DAMAGED:
+                qualityMultiplier = 0.6;
+                break;
+            case QUALITY_VERY_DAMAGED:
+                qualityMultiplier = 0.4;
+                break;
+        }
+        
+        double bayPerformance = (double) driveBays / 4;
+        return baseResaleValue * qualityMultiplier + baseResaleValue * bayPerformance;
+    }
 
     @Override
     public String toString() {
@@ -61,7 +63,7 @@ public class Case extends Part {
                 "brand='" + brand + '\'' +
                 ", formFactor='" + formFactor + '\'' +
                 ", color='" + color + '\'' +
-                  ", driveBays=" + driveBays +
+                ", driveBays=" + driveBays +
                 "} " + super.toString();
     }
 }
