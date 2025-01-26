18bf9f7363e6ea2117afbaa17c5283b276ec0c51
TheCrumbs <ryanwuwlm@gmail.com>
2025-01-26 14:57:02 -0500

    Update Blackjack.java
    Fixed inconsistent indentation across all classes
    
    Removed redundant whitespace and aligned class members properly
    
    Fixed parameter naming convention in setters (removed uppercase)
    
    Improved isEmpty() check in dealCard()
    
    Added consistent spacing around method parameters and braces
    
    Ensured all methods have consistent spacing between them
    
    Corrected field declarations to be at the same indentation level
    
    Fixed brace placement consistency

--- shop/Blackjack.java
+++ shop/Blackjack.java
@@ -4,95 +4,97 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 class Card {
-	private String suit;
-	    private String rank;
+    private String suit;
+    private String rank;
 
-	    public Card(String suit, String rank) {
-	        this.suit = suit;
-	        this.rank = rank;
-	    }
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
 
-	    public String getSuit() {
-	        return suit;
-	    }
-	    public void setSuit(String Suit) {
-	    	this.suit = Suit;
-	    }
-	    
-	    public String getRank() {
-	        return rank;
-	    }
-	    public void setRank(String Rank) {
-	    	this.rank = Rank;
-	    }
-	    
-	    public String toString() {
-	        return rank + " of " + suit;
-	    }
-	}
+    public String getSuit() {
+        return suit;
+    }
 
-	class Deck {
-	    private ArrayList<Card> cards;
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
 
-	    public Deck() {
-	        cards = new ArrayList<>();
-	        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
-	        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    public String getRank() {
+        return rank;
+    }
 
-	        for (String suit : suits) {
-	            for (String rank : ranks) {
-	                cards.add(new Card(suit, rank));
-	            }
-	        }
-	        shuffle();
-	    }
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
 
-	    public void shuffle() {
-	        Collections.shuffle(cards);
-	    }
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
 
-	    public Card dealCard() {
-	        if (cards.size() > 0) {
-	            return cards.remove(0);
-	        }
-	        return null;
-	    }
-	}
+class Deck {
+    private ArrayList<Card> cards;
 
-	class Player {
-	    private ArrayList<Card> hand;
+    public Deck() {
+        cards = new ArrayList<>();
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 
-	    public Player() {
-	        hand = new ArrayList<>();
-	    }
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+        shuffle();
+    }
 
-	    public void addCard(Card card) {
-	        hand.add(card);
-	    }
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
 
-	    public int calculateHandValue() {
-	        int value = 0;
-	        int aces = 0;
-	        for (Card card : hand) {
-	            String rank = card.getRank();
-	            if (rank.equals("Jack") || rank.equals("Queen") || rank.equals("King")) {
-	                value += 10;
-	            } else if (rank.equals("Ace")) {
-	                aces++;
-	                value += 11;
-	            } else {
-	                value += Integer.parseInt(rank);
-	            }
-	        }
+    public Card dealCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0);
+        }
+        return null;
+    }
+}
 
-	        while (value > 21 && aces > 0) {
-	            value -= 10;
-	            aces--;
-	        }
-	        return value;
-	    }
+class Player {
+    private ArrayList<Card> hand;
 
-	    public ArrayList<Card> getHand() {
-	        return hand;
-	    }
-	}
+    public Player() {
+        hand = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public int calculateHandValue() {
+        int value = 0;
+        int aces = 0;
+        for (Card card : hand) {
+            String rank = card.getRank();
+            if (rank.equals("Jack") || rank.equals("Queen") || rank.equals("King")) {
+                value += 10;
+            } else if (rank.equals("Ace")) {
+                aces++;
+                value += 11;
+            } else {
+                value += Integer.parseInt(rank);
+            }
+        }
+
+        while (value > 21 && aces > 0) {
+            value -= 10;
+            aces--;
+        }
+        return value;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+}
